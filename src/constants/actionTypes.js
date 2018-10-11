7ac87bdfb457a5c842ab56f26f838ac9d4dc68ed
Kayode Adeola <monsieurkayode@gmail.com>
2018-10-11 11:30:49 +0100

    Flux up application by creating action types constants

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,9 @@
+var keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({
+  CREATE_AUTHOR: null,
+  GET_AUTHORS: null,
+  INITIALIZE: null,
+  UPDATE_AUTHOR: null,
+  DELETE_AUTHOR: null
+});
